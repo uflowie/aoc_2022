bbed67a4158b45096947877336bca071b235e5ca
florin-ulrich <florin.n.ulrich@gmail.com>
2022-12-08 17:10:53 +0100

    input files are now always addressed from parent directory

--- start_new_puzzle.py
+++ start_new_puzzle.py
@@ -22,7 +22,7 @@ os.mkdir(day)
 
 # create file containing the code for the problem, including the default way to read in the puzzle input
 with open(os.path.join(day, f"{day}_solution.py"), "w") as solution_file:
-    solution_file.write("with open('input.txt', 'r') as input_file:\n")
+    solution_file.write(f"with open('{day}/input.txt', 'r') as input_file:\n")
     solution_file.write("\tpuzzle_input = input_file.read().splitlines()\n")
 
 with open(os.path.join(day, "input.txt"), "w") as input_file:
